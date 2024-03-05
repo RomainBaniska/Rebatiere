40ceb1ab127103ec778880425ee208241b5299ed
RomainBaniska <132657704+RomainBaniska@users.noreply.github.com>
2024-03-05 01:54:39 +0100

    Premier test ok avec la base de donnée
    enregistrement/{id}/edit chope bien l'utilisateur dans la base de données

--- Rebatiere/src/Controller/UserController.php
+++ Rebatiere/src/Controller/UserController.php
@@ -2,16 +2,27 @@
 
 namespace App\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
+use App\Entity\Users;
+use App\Repository\UsersRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UserController extends AbstractController
 {
     #[Route('/enregistrement', name: 'app_enregistrement')]
-// dd
-    
+    public function register(EntityManagerInterface $entityManager, UsersRepository $user): Response
+    {
+        $entityManager->getConnection()->connect();
+    }
+
+    #[Route('/enregistrement/{id}/edit', name: 'app_enregistrement_edit')]
+    public function editUser(EntityManagerInterface $entityManager, Users $user): Response
+    {
+        dd($user);
+        $entityManager->getConnection()->connect();
+    }
 
     #[Route('/dbTestConnection', name: 'app_dbTestConnection')]
     public function testDatabaseConnection(EntityManagerInterface $entityManager): Response
