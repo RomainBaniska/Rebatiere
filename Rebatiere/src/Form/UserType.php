5be05779ff0c434b8e89674ff63eda37460ae9ce
RomainBaniska <132657704+RomainBaniska@users.noreply.github.com>
2024-03-10 20:11:42 +0100

    modifications formtype

--- Rebatiere/src/Form/UserType.php
+++ Rebatiere/src/Form/UserType.php
@@ -6,14 +6,19 @@ use App\Entity\Users;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 class UserType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('username')
+            ->add('username', TextType::class, [
+                'label' => 'Nom d\'utilisateur'
+            ])
             ->add('password')
+            ->add('save', SubmitType::class)
             ;
     }
 
