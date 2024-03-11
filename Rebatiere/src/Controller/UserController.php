cd09c1e87cc7bb643c8468c05d23bf7d52e733da
RomainBaniska <132657704+RomainBaniska@users.noreply.github.com>
2024-03-11 10:21:19 +0100

    Controller Commit

--- Rebatiere/src/Controller/UserController.php
+++ Rebatiere/src/Controller/UserController.php
@@ -6,6 +6,7 @@ use App\Entity\Users;
 use App\Form\UserType;
 use App\Repository\UsersRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -21,11 +22,16 @@ class UserController extends AbstractController
     }
 
     #[Route('/enregistrement/{id}/edit', name: 'app_enregistrement_edit')] // on peut remplacer id par n'importe quel autre champs
-    public function editUser(EntityManagerInterface $entityManager, Users $user): Response
+    public function editUser(EntityManagerInterface $em, Users $user, Request $request): Response
     {
         $form = $this->createForm(UserType::class, $user);
+        $form->handleRequest($request);
 
-
+        if($form->isSubmitted() && $form->isValid()) {
+            $em->flush(); // on appelle l'entityManager pour "flush" (save/valider) en BDD
+            $this->addFlash('success', 'L\'utilisateur a bien été modifié');
+            // return $this->redirectToRoute('');
+        }
         // dd($user); 
         return $this->render('user/edit.html.twig', [
             'user' => $user,
