53b04831c3815ac1c4081c4b874090c81b53f38a
RomainBaniska <132657704+RomainBaniska@users.noreply.github.com>
2024-03-22 10:22:24 +0100

    Commit quick

--- Rebatiere/src/Controller/Admin/DashboardController.php
+++ Rebatiere/src/Controller/Admin/DashboardController.php
@@ -7,6 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 
 class DashboardController extends AbstractDashboardController
@@ -15,6 +16,9 @@ class DashboardController extends AbstractDashboardController
     #[IsGranted('ROLE_ADMIN')]
     public function index(): Response
     {
+        if (!$this->isGranted('ROLE_ADMIN')) {
+            return $this->render('403/dashboard.html.twig');
+        }
         // return parent::index();
 
         return $this->render('admin/dashboard.html.twig');
