c9bd63cec8671dcfe470e339db527bf187115f42
RomainBaniska <132657704+RomainBaniska@users.noreply.github.com>
2024-05-20 23:23:23 +0200

    CALENDAR CONTROLLER & DATAS

--- Rebatiere/src/Controller/CalendarController.php
+++ Rebatiere/src/Controller/CalendarController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Reservation;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class CalendarController extends AbstractController
 {
     #[Route('/calendar', name: 'app_calendar')]
-    public function index(): Response
+    public function index(EntityManagerInterface $em): Response
     {
-        return $this->render('calendar/index.html.twig', [
+        $events = $em->getRepository(Reservation::class)->findall();
+    
+
+        return $this->render('calendar/eventslist.html.twig', [
             'controller_name' => 'CalendarController',
+            'events' => $events,
         ]);
     }
 }
