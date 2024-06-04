cd3aa9104f519dc0a7e11e3f423b2219e4fbfca7
RomainBaniska <132657704+RomainBaniska@users.noreply.github.com>
2024-06-04 14:24:26 +0200

    Events dans un tableau

--- Rebatiere/src/Controller/CalendarController.php
+++ Rebatiere/src/Controller/CalendarController.php
@@ -14,35 +14,35 @@ class CalendarController extends AbstractController
     #[Route('/calendar', name: 'app_calendar')]
     public function index(EntityManagerInterface $em): Response
     {
-        $events = $em->getRepository(Reservation::class)->findAll();
+        // $events = $em->getRepository(Reservation::class)->findAll();
     
-        // Initialisez le tableau pour stocker tous les événements
-        $calendarEvents = [];
+        // // Initialisez le tableau pour stocker tous les événements
+        // $calendarEvents = [];
 
-        // Boucle sur tous les événements pour récupérer les données et les afficher
-        foreach ($events as $event) {
+        // // Boucle sur tous les événements pour récupérer les données et les afficher
+        // foreach ($events as $event) {
             
-            $calendarEvent = [
-                // 'title' => $event->getUsers()->getUsername(),
-                'start' => $event->getStart()->format('Y-m-d\TH:i:s'),
-                'end' => $event->getEnd()->format('Y-m-d\TH:i:s'),
-                'color' => '#AB351C',
-                // 'color' => 'transparent',
-                'extendedProps' => [
-                    'icon' => '/uploads/images/' . $event->getUsers()->getImageFileName()
-                    // Rajouter une condition si null
-                ],
-            ];
-            $calendarEvents[] = $calendarEvent;
-        }
+        //     $calendarEvent = [
+        //         // 'title' => $event->getUsers()->getUsername(),
+        //         'start' => $event->getStart()->format('Y-m-d\TH:i:s'),
+        //         'end' => $event->getEnd()->format('Y-m-d\TH:i:s'),
+        //         'color' => '#AB351C',
+        //         // 'color' => 'transparent',
+        //         'extendedProps' => [
+        //             'icon' => '/uploads/images/' . $event->getUsers()->getImageFileName()
+        //             // Rajouter une condition si null
+        //         ],
+        //     ];
+        //     $calendarEvents[] = $calendarEvent;
+        // }
 
-        $datas = json_encode($calendarEvents);
+        // $datas = json_encode($calendarEvents);
 
         // dump($datas);
 
         return $this->render('calendar/eventslist.html.twig', [
             'controller_name' => 'CalendarController',
-            'datas' => $datas,
+            // 'datas' => $datas,
         ]);
     }
 }
