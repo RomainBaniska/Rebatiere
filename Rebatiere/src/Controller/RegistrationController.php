b30db361abe090bf4bbe5448410bc370fb63746b
RomainBaniska <132657704+RomainBaniska@users.noreply.github.com>
2024-08-17 17:29:29 +0200

    Correction registration (null) & un peu de css

--- Rebatiere/src/Controller/RegistrationController.php
+++ Rebatiere/src/Controller/RegistrationController.php
@@ -37,7 +37,7 @@ class RegistrationController extends AbstractController
                 $fileName = uniqid().'.'.$photo->guessExtension();
                 $photo->move($photoDir, $fileName);
             }
-            $user->setImageFileName(null);
+            $user->setImageFileName($fileName);
 
 
             // Persist & Flush en BDD
