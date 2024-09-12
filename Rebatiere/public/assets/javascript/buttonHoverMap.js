9a93c1d0dd1b31d9e1f236fcb8bf661505d53762
RomainBaniska <132657704+RomainBaniska@users.noreply.github.com>
2024-09-12 20:02:29 +0200

    animation avance
    mais problème au niveau de la remise du z-index à 4

--- Rebatiere/public/assets/javascript/buttonHoverMap.js
+++ Rebatiere/public/assets/javascript/buttonHoverMap.js
@@ -43,34 +43,36 @@ Object.keys(boutons).forEach(function(boutonKey, index) {
 
     if (bouton && chambre) {
         bouton.addEventListener('mouseenter', function() {
-            chambre.classList.remove('zoom-animation2');
+
+            // Lorsque la souris entre sur le bouton, les éléments ont un zoom.
+            chambre.classList.remove('zoom-animationEnd');
             chambre.classList.add('zoom-animation');
-            // Augmenter le z-index de la chambre & fullBlackShape lors de l'animation
             chambre.style.zIndex = 9999;
-            // Afficher layer4 lors du survol
-            layer4.style.display = 'block';
-            layer4.classList.remove('shadowing-animation2')
+
+            layer4.classList.remove('shadowing-animationEnd');
             layer4.classList.add('shadowing-animation');
+             
         });
 
         bouton.addEventListener('mouseleave', function() {
+            
             chambre.classList.remove('zoom-animation');
-            chambre.classList.add('zoom-animation2');
-            // Réinitialiser le z-index de la chambre & fullBlackShape en fin d'animation
-            chambre.style.zIndex = 4;
-            // Masquer layer4 lorsque le survol s'arrête
-            layer4.classList.remove('shadowing-animation');
-            layer4.classList.add('shadowing-animation2')
+            chambre.classList.add('zoom-animationEnd');
 
-            // Écouter la fin de l'animation avant de cacher layer4
-            layer4.addEventListener('animationend', function handleAnimationEnd() {
-                // Vérifier si l'animation terminée est bien 'shadowing-animation2'
-                if (layer4.classList.contains('shadowing-animation2')) {
-                    layer4.style.display = 'none';
-                    layer4.removeEventListener('animationend', handleAnimationEnd); // Nettoyer l'écouteur
-                }
-            });
-            // layer4.style.display = 'none';
+
+            chambre.style.zIndex = 4;
+
+            // // On attend la fin de l'animation de déZoom avant de refaire passer l'élément en z-index 4
+            // chambre.addEventListener('animationend', function onAnimationEnd(event) {
+            //     if (event.animationName === 'zoom-animationEnd') {
+            //         chambre.style.zIndex = 4; 
+            //         chambre.removeEventListener('animationend', onAnimationEnd); // Supprimer l'écouteur
+            //     }
+            // });
+
+
+            layer4.classList.remove('shadowing-animation');
+            layer4.classList.add('shadowing-animationEnd');
         });
     }
 });
