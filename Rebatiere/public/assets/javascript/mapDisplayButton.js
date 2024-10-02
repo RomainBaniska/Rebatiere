d4635616d596a66461e0bb599112c6df7d5027a8
RomainBaniska <132657704+RomainBaniska@users.noreply.github.com>
2024-10-02 16:51:56 +0200

    centering du formsheet
    à continuer

--- Rebatiere/public/assets/javascript/mapDisplayButton.js
+++ Rebatiere/public/assets/javascript/mapDisplayButton.js
@@ -1,24 +1,29 @@
 const button = document.getElementById('toggleButton');
 const box = document.querySelector('.box');
+const map = document.querySelector('.map');
 const formContainer = document.getElementById('formSheetContainer');
 
-// Fonction pour gérer l'extension et la rétractation du formContainer et l'ouverture/fermeture de la map
 button.addEventListener('click', () => {
     if (box.classList.contains('show')) {
-        // Si la map est visible, on la ferme avant de rétracter le formContainer
         box.classList.remove('show');
         box.classList.add('hide');
+
         
         setTimeout(() => {
             box.style.visibility = 'hidden';
             box.classList.remove('hide');
             
-            // Une fois la map fermée, on rétracte le formContainer
             formContainer.classList.remove('expanded');
+            formContainer.style.justifyContent = "center";
+            map.style.display = "none";
+
+
+
         }, 350); // Correspond à la durée de l'animation de fermeture de la map
     } else {
-        // Étendre d'abord le formContainer avant d'ouvrir la map
         formContainer.classList.add('expanded');
+        formContainer.style.justifyContent = "flex-start";
+        map.style.display = "block";
         
         setTimeout(() => {
             box.style.visibility = 'visible';
