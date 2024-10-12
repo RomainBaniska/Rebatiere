5c5bc7d266212880555efe9fe5183a43fd50fcf3
RomainBaniska <132657704+RomainBaniska@users.noreply.github.com>
2024-10-12 22:33:32 +0200

    Extension page pour "users"

---
+++ Rebatiere/public/assets/javascript/usersDisplayButton.js
@@ -0,0 +1,48 @@
+// const formContainer = document.getElementById('formSheetContainer');
+// const formSheet = document.getElementById('formSheet');
+const membersBox = document.querySelector('.membersBox');
+const members = document.querySelector('.members');
+const buttonMembers = document.getElementById('toggleMembers');
+let isAnimatingMembers = false;
+
+buttonMembers.addEventListener('click', () => {
+
+    if (isAnimatingMembers) return;
+
+    isAnimatingMembers = true;
+
+    if (membersBox.classList.contains('show')) {
+        membersBox.classList.remove('show');
+        membersBox.classList.add('hide');
+
+        
+        setTimeout(() => {
+            membersBox.style.visibility = 'hidden';
+            membersBox.classList.remove('hide');  
+
+            formContainer.classList.remove('expanded');
+            members.style.display = "none";
+
+            formSheet.classList.add('recenter');
+
+            setTimeout(() => {
+                formContainer.classList.remove('position');
+                formSheet.classList.remove('recenter');
+                buttonMembers.innerHTML=">";
+                isAnimatingMembers = false; 
+            }, 1500);
+
+        }, 350); 
+    } else {
+        formContainer.classList.add('expanded');
+        formContainer.classList.add('position');
+        members.style.display = "block";
+        
+        setTimeout(() => {
+            membersBox.style.visibility = 'visible';
+            membersBox.classList.add('show');
+            buttonMembers.innerHTML="<";
+            isAnimatingMembers = false;
+        }, 350);
+    }
+});
