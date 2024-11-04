9d73484dd4ee899517ec1af440f8feeff6feb57d
RomainBaniska <132657704+RomainBaniska@users.noreply.github.com>
2024-11-04 05:43:27 +0100

    création du menu dynamique plus sympa

--- Rebatiere/public/assets/javascript/headerMenu.js
+++ Rebatiere/public/assets/javascript/headerMenu.js
@@ -5,15 +5,30 @@ var profileButton = document.querySelector(".profile-pic");
 var logoutButton = document.querySelector(".profile-logout");
 
 profileButton.addEventListener("click", () => {
-    if (!reservationButton.classList.contains('show')) {
-        reservationButton.classList.add('show');
-        editButton.classList.add('show');
-        messageButton.classList.add('show');
-        logoutButton.classList.add('show');
-    } else {
+    if (reservationButton.classList.contains('show')) {
+
         reservationButton.classList.remove('show');
         editButton.classList.remove('show');
         messageButton.classList.remove('show');
-        logoutButton.classList.remove('show');
+        logoutButton.classList.remove('showLogout');
+
+        reservationButton.classList.add('hide');
+        editButton.classList.add('hide');
+        messageButton.classList.add('hide');
+        logoutButton.classList.add('hideLogout');
+
+        setTimeout(() => {
+        reservationButton.classList.remove('hide');
+        editButton.classList.remove('hide');
+        messageButton.classList.remove('hide');
+        logoutButton.classList.remove('hideLogout');
+    }, 500)
+        
+    } else {
+        reservationButton.classList.add('show');
+        editButton.classList.add('show');
+        messageButton.classList.add('show');
+        logoutButton.classList.add('showLogout');
+  
     }
 });
