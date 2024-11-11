1468759470741e99d0d516defc0947ef620c5fd8
RomainBaniska <132657704+RomainBaniska@users.noreply.github.com>
2024-11-11 23:01:36 +0100

    Le cropper fonctionne !
    J'ai dû refaire un formulaire complet à la main, le fichier est injecté dans le formulaire et toutes les infos sont récupérées!

--- Rebatiere/public/assets/javascript/cropperProfile.js
+++ Rebatiere/public/assets/javascript/cropperProfile.js
@@ -5,6 +5,7 @@ const profileImageInput = document.getElementById("profileImageInput");
 const imageToCrop = document.getElementById("imageToCrop");
 const cropOutput = document.getElementById('cropOutput');
 const cropImageBtn = document.getElementById('cropImageBtn');
+const croppedImageJPGFile = document.getElementById('croppedImageJPGFile');
 const reader = new FileReader();
 
 // On initialise le cropper et la croppedImage
@@ -62,20 +63,39 @@ let croppedImage = null;
             }
             const blob = new Blob([bytes], { type: 'image/jpg'});
 
-            // On génère une URL pour le blob
-            const url = URL.createObjectURL(blob);
+            // Créer un fichier à partir du blob
+            const file = new File([blob], "croppedImage.jpg", { type: 'image/jpg' });
 
-            // On télécharge l'image 
-            const a = document.createElement('a');
-            a.href = url;
-            a.download = 'image.jpg';
-            console.log(a);
-            console.log('OK!');
-            a.style.display = 'none'; // On cache l'élément dans le DOM
-            document.body.appendChild(a); // On l'ajoute temporairement au document
-            a.click(); // On déclenche le clic pour télécharger l'image
-            document.body.removeChild(a); // On le supprime ensuite du DOM
-            URL.revokeObjectURL(url); // On libère la mémoire de l'URL
+            // On utilise DataTransfer pour injecter le fichier dans le champ de téléchargement
+            const dataTransfer = new DataTransfer();
+            dataTransfer.items.add(file);
+
+            // Injection du fichier JPG dans l'input 
+            croppedImageJPGFile.files = dataTransfer.files;
+
+            console.log (croppedImageJPGFile.files);
+            // return;
+
+        // Utilisation d'un délai pour s'assurer que le fichier est bien injecté
+        setTimeout(() => {
+            form.submit();  // Soumettre le formulaire après l'injection du fichier
+        }, 4000);
+
+
+            // // On génère une URL pour le blob
+            // const url = URL.createObjectURL(blob);
+
+            // // On télécharge l'image 
+            // const a = document.createElement('a');
+            // a.href = url;
+            // a.download = 'image.jpg';
+            // console.log(a);
+            // console.log('OK!');
+            // a.style.display = 'none'; // On cache l'élément dans le DOM
+            // document.body.appendChild(a); // On l'ajoute temporairement au document
+            // a.click(); // On déclenche le clic pour télécharger l'image
+            // document.body.removeChild(a); // On le supprime ensuite du DOM
+            // URL.revokeObjectURL(url); // On libère la mémoire de l'URL
             return;
 
 
--- Rebatiere/src/Controller/RegistrationController.php
+++ Rebatiere/src/Controller/RegistrationController.php
@@ -5,13 +5,13 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\RegistrationFormType;
 use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Bundle\SecurityBundle\Security;
-use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class RegistrationController extends AbstractController
 {
@@ -24,10 +24,25 @@ class RegistrationController extends AbstractController
 
 
         if ($form->isSubmitted() && $form->isValid()) {
+
+            $username = $form->get('username')->getData();
+            $firstname = $form->get('firstname')->getData();
+            $lastname = $form->get('lastname')->getData();
             $password = $form->get('plainPassword')->getData();
-            $user->setPassword(
-                $userPasswordHasher->hashPassword($user, $password)
-            );
+
+            // Récupérer le fichier envoyé (image de profil)
+            $croppedImage = $request->files->get('registrationForm')['croppedImageJPGFile'];
+
+            // $randomText = $request->get('randomText')->getData();
+
+                    // Afficher les informations pour déboguer
+            dump($username, $firstname, $lastname, $password);
+            dump($croppedImage);  // Cela vous montre le fichier téléchargé
+            exit();
+
+            // $user->setPassword(
+            //     $userPasswordHasher->hashPassword($user, $password)
+            // );
 
 
             // // Ajout de l'avatar en BDD avec la condition de null
@@ -37,30 +52,6 @@ class RegistrationController extends AbstractController
             //        $croppedImage->move($photoDir, $filename);
             //        $user->setImageFileName($filename);
             // }
-
-            // Récupérer l'image en base64
-            $croppedImageBase64 = $request->get('croppedImage'); // Supposons que l'image soit envoyée via un champ caché
-
-            dump($croppedImageBase64);
-            exit();
-
-            if ($croppedImageBase64) {
-                // Décoder l'image en base64
-                $data = base64_decode(preg_replace('/^data:image\/\w+;base64,/', '', $croppedImageBase64));
-
-                // Générer un nom unique pour le fichier
-                $filename = uniqid() . '.png'; // Ou l'extension correspondant à l'image
-
-                // Enregistrer l'image décodée dans un fichier
-                file_put_contents($photoDir . '/' . $filename, $data);
-
-                // Assigner le nom du fichier à l'utilisateur
-                $user->setImageFileName($filename);
-            }
-
-            // dump($user);
-            // dump($croppedImage);
-            // exit();
 
             // Persist & Flush en BDD
             $entityManager->persist($user);
@@ -72,6 +63,54 @@ class RegistrationController extends AbstractController
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form,
         ]);
+    }
+
+    #[Route('/register2', name: 'app_register2')]
+    public function register2(): Response
+    {
+        return $this->render('registration/register2.html.twig');
+    }
+
+
+    #[Route('/persistregistration', name: 'app_persistregistration')]
+    public function persistRegistration(Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $userPasswordHasher, #[Autowire('%photo_dir%')] string $photoDir): Response
+    {
+            $username = $request->request->get('username');
+            $firstname = $request->request->get('firstname');
+            $lastname = $request->request->get('lastname');
+            $plainPassword = $request->request->get('plainPassword');
+            $croppedImage = $request->files->get('croppedImageJPGFile');
+
+            // Je crée une nouvelle instance d'User
+            $user = new User();
+
+            // Hachage de password
+            // $hashedPassword = $user->setPassword (
+            //         $userPasswordHasher->hashPassword($user, $plainPassword)
+            // );
+            $hashedPassword = $userPasswordHasher->hashPassword($user, $plainPassword);
+            $user->setPassword($hashedPassword);
+
+            // Ajout de l'avatar en BDD avec la condition de null
+            $fileName = null;
+            // $croppedImage = $request->files->get('croppedImageJPGFile'); 
+            if(isset($croppedImage)) {
+                $fileName = uniqid().'.'.$croppedImage->guessExtension();
+                $croppedImage->move($photoDir, $fileName);
+            }
+
+            // Assignation des informations traitées
+            $user->setUsername($username);
+            $user->setFirstname($firstname);
+            $user->setLastname($lastname);
+            $user->setPassword($hashedPassword);
+            $user->setImageFileName($fileName);
+
+            // Persist & Flush en BDD
+            $em->persist($user);
+            $em->flush();
+
+            return new Response('<h1>Formulaire soumis avec succès.</h1>');
     }
 }
 
